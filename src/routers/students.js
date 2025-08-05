1060b3cca16f6b54f8cc8888e74051ccf8e5781b
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-08-05 13:04:47 +0300

    Add PUT, PATCH, DELETE support via new services and controllers

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
 import {
+  createStudentController,
+  deleteStudentController,
   getStudentByIdController,
   getStudentsController,
+  patchSrudentController,
+  upsertStudentController,
 } from '../controllers/students.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -13,3 +17,11 @@ export default router;
 router.get('/students', ctrlWrapper(getStudentsController));
 
 router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
+
+router.post('/students', ctrlWrapper(createStudentController));
+
+router.delete('/students/:studentId', ctrlWrapper(deleteStudentController));
+
+router.put('/students/:studentId', ctrlWrapper(upsertStudentController));
+
+router.patch('/students/:studentId', ctrlWrapper(patchSrudentController));
--- src/server.js
+++ src/server.js
@@ -19,7 +19,12 @@ export const startServer = () => {
     }),
   );
 
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
   app.use(cors());
 
   app.use(studentsRouter);
