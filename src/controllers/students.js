61f1ad3146df71b5ed994dddfb26840b90d7fb9f
Vyacheslav chopenko <syavchik260119901@gmail.com>
2025-08-15 19:29:03 +0300

    add sorting for students

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -7,12 +7,16 @@ import {
   updateStudent,
 } from '../services/students.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getStudentsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
   const students = await getAllStudents({
     page,
     perPage,
+    sortBy,
+    sortOrder,
   });
   res.json({
     status: 200,
